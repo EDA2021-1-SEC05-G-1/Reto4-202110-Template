deedf6e50c520e0ac7482c859bfbb5eff540503f
ValeGoyeneche <m.goyenechea@uniandes.edu.co>
2021-05-19 23:40:32 -0500

    lab 10

--- App/controller.py
+++ App/controller.py
@@ -21,8 +21,59 @@
  """
 
 import config as cf
-import model
+from App import model
 import csv
+# ___________________________________________________
+#  Inicializacion del catalogo
+# ___________________________________________________
+
+
+def init():
+    """
+    Llama la funcion de inicializacion  del modelo.
+    """
+    # analyzer es utilizado para interactuar con el modelo
+    analyzer = model.newAnalyzer()
+    return analyzer
+
+
+# ___________________________________________________
+#  Funciones para la carga de datos y almacenamiento
+#  de datos en los modelos
+# ___________________________________________________
+
+
+def loadServices(analyzer, connectionsfile, landing_points_file, countriesfile):
+    """
+    Carga los datos de los archivos CSV en el modelo.
+    Se crea un arco entre cada par de estaciones que
+    pertenecen al mismo servicio y van en el mismo sentido.
+
+    addRouteConnection crea conexiones entre diferentes rutas
+    servidas en una misma estación.
+    """
+    connectionsfile = cf.data_dir + connectionsfile
+    input_file1 = csv.DictReader(open(connectionsfile, encoding="utf-8"),
+                                delimiter=",")
+    landing_points_file = cf.data_dir + landing_points_file
+    input_file2 = csv.DictReader(open(landing_points_file, encoding="utf-8"),
+                                delimiter=",")
+    countriesfile = cf.data_dir + countriesfile
+    input_file3 = csv.DictReader(open(countriesfile, encoding="utf-8"),
+                                delimiter=",")
+
+    lastcable = None
+    for cable in input_file1:
+        if lastcable is not None:
+            sameservice = lastcable[0]['cable_id'] == cable['cable_id']
+            samedirection = lastcable[0]['destination'] == cable['destination']
+            for cable2 in input_file2:
+                samebusStop = lastcable[1]['landing_point_id'] == cable2['landing_point_id']#Vertice
+            if sameservice and samedirection and not samebusStop:
+                model.addStopConnection(analyzer, lastcable, cable, cable2)
+        lastcable = [cable, cable2]
+    model.addRouteConnections(analyzer)
+    return analyzer
 
 
 """
--- App/view.py
+++ App/view.py
@@ -20,12 +20,12 @@
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  """
 
-import config as cf
 import sys
-import controller
-from DISClib.ADT import list as lt
-assert cf
-
+import config
+import threading
+from App import controller
+from DISClib.ADT import stack
+assert config
 
 """
 La vista se encarga de la interacción con el usuario
@@ -33,26 +33,63 @@ Presenta el menu de opciones y por cada seleccion
 se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
+connectionsfile = 'connections.csv'
+countriesfile = 'countries.csv'
+landing_points_file = 'landing_points.csv'
+initialStation = None
 
 def printMenu():
+    print("\n")
+    print("*******************************************")
     print("Bienvenido")
-    print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("1- Inicializar Analizador")
+    print("2- Cargar información de los landing points")
+    print("3- Conocer si dos landing points estan en el mismo cluster")
+    print("4- Encontrar el (los) landing point(s) que sirven como punto de interconexión a más cables en la red")
+    print("5- Encontrar la ruta mínima en distancia para enviar información entre dos paises")
+    print("6- Identificar la red de expansión mínima en distancias que llegue a más landing points")
+    print("7- Cuántos y cuáles son los países afectados con la caída del landing point")
+    print("0- Salir")
+    print("*******************************************")
 
-catalog = None
+
+def optionTwo(cont):
+    print("\nCargando información de transporte de singapur ....")
+    controller.loadServices(cont, connectionsfile, landing_points_file, countriesfile)
+    numedges = controller.totalConnections(cont)
+    numvertex = controller.totalStops(cont)
+    print('Numero de vertices: ' + str(numvertex))
+    print('Numero de arcos: ' + str(numedges))
+    print('El limite de recursion actual: ' + str(sys.getrecursionlimit()))
+
+
+def optionThree(cont):
+    print('El número de componentes conectados es: ' +
+          str(controller.connectedComponents(cont)))
+
+
 
 """
 Menu principal
 """
-while True:
-    printMenu()
-    inputs = input('Seleccione una opción para continuar\n')
-    if int(inputs[0]) == 1:
-        print("Cargando información de los archivos ....")
+def thread_cycle():
+    while True:
+        printMenu()
+        inputs = input('Seleccione una opción para continuar\n>')
 
-    elif int(inputs[0]) == 2:
-        pass
+        if int(inputs[0]) == 1:
+            print("\nInicializando....")
+            # cont es el controlador que se usará de acá en adelante
+            cont = controller.init()
 
+        elif int(inputs[0]) == 2:
+            optionTwo(cont)
     else:
         sys.exit(0)
 sys.exit(0)
+
+if __name__ == "__main__":
+    threading.stack_size(67108864)  # 64MB stack
+    sys.setrecursionlimit(2 ** 20)
+    thread = threading.Thread(target=thread_cycle)
+    thread.start()
